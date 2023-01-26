376198d6c023185cf9190e3bbe916e03fabaabc2
Vladyslav Marushchak <marushchakvl@icloud.com>
2023-01-26 16:07:46 +0200

    task done

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,11 @@
+const URL = 'https://restcountries.com/v3.1/name/';
+const FILTER = '?fields=name,capital,population,flags,languages';
+
 export function fetchCountries(name) {
-   return fetch(
-     `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-   ).then(response => {
-     if (!response.ok) {
-       throw new Error(response.status);
-     }
-     return response.json();
-   });
- }
+  return fetch(`${URL}${name}${FILTER}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
